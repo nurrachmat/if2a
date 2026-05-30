bfe7f25fbf7dd2c5e83a7b0176e155d899849539
nurrachmat <rachmat.nur@gmail.com>
2026-05-30 11:13:38 +0700

    menambahkan grafik highcharts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PeriodeController;
@@ -22,3 +23,5 @@ Route::resource('fakultas', FakultasController::class)
 Route::resource('/periode', PeriodeController::class);
 Route::resource('/prodi', ProdiController::class);
 Route::resource('/mahasiswa', MahasiswaController::class);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
