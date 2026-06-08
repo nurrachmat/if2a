24bf0aa334ab0c62762220df853269485af1883e
nurrachmat <rachmat.nur@gmail.com>
2026-06-08 15:03:46 +0700

    update web.php

--- routes/web-backup.php
+++ routes/web-backup.php
@@ -5,14 +5,17 @@ use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\ProdiController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
-    return redirect()->route('login');
+    return view('welcome');
 });
 
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+
+// Route::resource('/fakultas', FakultasController::class);
 Route::resource('fakultas', FakultasController::class)
     ->parameters([
         'fakultas' => 'fakultas'
@@ -21,12 +24,4 @@ Route::resource('/periode', PeriodeController::class);
 Route::resource('/prodi', ProdiController::class);
 Route::resource('/mahasiswa', MahasiswaController::class);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/dashboard', [DashboardController::class, 'index']);
